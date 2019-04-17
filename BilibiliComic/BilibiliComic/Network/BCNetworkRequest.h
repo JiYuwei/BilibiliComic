4418b796b4e43413aebe0207ac11a24fb6e3a531
JiYuwei <344870729@qq.com>
2019-04-17 18:30:32 +0800

    Go ahead

--- BilibiliComic/BilibiliComic/Network/BCNetworkRequest.h
+++ BilibiliComic/BilibiliComic/Network/BCNetworkRequest.h
@@ -20,21 +20,46 @@ typedef void(^requestSuccessBlock) (NSDictionary *json);
 typedef void(^requestFailureBlock) (NSError *error,BOOL needCache,NSDictionary *cachedJson);
 typedef void(^failureBlock) (NSError *error);
 
-
 @interface BCNetworkRequest : NSObject
 
+/**
+ GET请求
+
+ @param url         请求的URL
+ @param parameters  参数
+ @param success     成功回调
+ @param failure     失败回调
+ */
 +(void)retrieveJsonUseGETfromURL:(NSString *)url
                       parameters:(NSDictionary *)parameters
                          success:(requestSuccessBlock)success
                          failure:(requestFailureBlock)failure;
 
+/**
+ POST请求
+
+ @param url         请求的URL
+ @param parameters  参数
+ @param success     成功回调
+ @param failure     失败回调
+ */
 +(void)retrieveJsonUsePOSTfromURL:(NSString *)url
                        parameters:(NSDictionary *)parameters
                           success:(requestSuccessBlock)success
                           failure:(requestFailureBlock)failure;
 
+/**
+ 通用请求
 
-
+ @param prepare     准备阶段回调
+ @param finish      请求完成回调
+ @param needCache   是否需要缓存
+ @param type        请求类型 GET/POST
+ @param url         请求的URL
+ @param parameters  参数
+ @param success     成功回调
+ @param failure     失败回调
+ */
 +(void)retrieveJsonWithPrepare:(prepareBlock)prepare
                         finish:(finishBlock)finish
                      needCache:(BOOL)needCache
@@ -44,8 +69,16 @@ typedef void(^failureBlock) (NSError *error);
                        success:(requestSuccessBlock)success
                        failure:(requestFailureBlock)failure;
 
+/**
+ 上传图片
 
-
+ @param url         上传图片URL
+ @param parameters  参数
+ @param image       图片
+ @param original    是否上传原图
+ @param success     成功回调
+ @param failure     失败回调
+ */
 +(void)uploadImageWithURL:(NSString *)url
                parameters:(NSDictionary *)parameters
                     image:(UIImage *)image
@@ -53,7 +86,17 @@ typedef void(^failureBlock) (NSError *error);
                   success:(requestSuccessBlock)success
                   failure:(failureBlock)failure;
 
+/**
+ 上传多图
 
+ @param url             上传图片URL
+ @param parameters      参数
+ @param images          图片数组
+ @param original        是否上传原图
+ @param uploadProgress  上传进度
+ @param success         成功回调
+ @param failure         失败回调
+ */
 +(void)uploadMutiImageWithURL:(NSString *)url
                       parameters:(NSDictionary *)parameters
                           images:(NSArray <UIImage *> *)images
@@ -62,7 +105,19 @@ typedef void(^failureBlock) (NSError *error);
                          success:(requestSuccessBlock)success
                          failure:(failureBlock)failure;
 
+/**
+ 上传文件
 
+ @param url             上传文件URL
+ @param parameters      参数
+ @param fileURL         文件路径
+ @param name            名称
+ @param fileName        文件名称
+ @param mimeType        媒体类型
+ @param uploadProgress  上传进度
+ @param success         成功回调
+ @param failure         失败回调
+ */
 +(void)uploadFileWithURL:(NSString *)url
               parameters:(NSDictionary *)parameters
                  fileURL:(NSURL *)fileURL
@@ -73,15 +128,29 @@ typedef void(^failureBlock) (NSError *error);
                  success:(requestSuccessBlock)success
                  failure:(failureBlock)failure;
 
+/**
+ 下载文件
 
+ @param url              下载文件URL
+ @param donloadProgress  下载进度
+ @param success          成功回调
+ @param failure          失败回调
+ */
 +(void)downloadFileWithURL:(NSString *)url
                   progress:(progressBlock)donloadProgress
                    success:(void (^)(NSURLResponse *response, NSURL *filePath))success
                    failure:(failureBlock)failure;
 
+/**
+ 取消指定URL的请求
 
+ @param url 需要取消请求的URL
+ */
 +(void)cancelRequestWithURL:(NSString *)url;
 
+/**
+ 取消所有请求
+ */
 +(void)cancelAllRequest;
 
 @end
---
+++ BilibiliComic/BilibiliComic/RequestAPI.h
@@ -0,0 +1,13 @@
+//
+//  RequestAPI.h
+//  BilibiliComic
+//
+//  Created by 纪宇伟 on 2019/4/17.
+//  Copyright © 2019 jyw. All rights reserved.
+//
+
+#ifndef RequestAPI_h
+#define RequestAPI_h
+
+
+#endif /* RequestAPI_h */
