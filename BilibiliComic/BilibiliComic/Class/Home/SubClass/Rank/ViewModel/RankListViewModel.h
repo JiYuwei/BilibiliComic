0d835861b1dd0f94eb0e36f1726d3ae52d5559fd
JiYuwei <344870729@qq.com>
2019-06-18 17:38:54 +0800

    done

---
+++ BilibiliComic/BilibiliComic/Class/Home/SubClass/Rank/ViewModel/RankListViewModel.h
@@ -0,0 +1,17 @@
+//
+//  RankListViewModel.h
+//  BilibiliComic
+//
+//  Created by 纪宇伟 on 2019/6/18.
+//  Copyright © 2019 jyw. All rights reserved.
+//
+
+#import "BaseViewModel.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RankListViewModel : BaseViewModel
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BilibiliComic/BilibiliComic/Class/Home/SubClass/Rank/ViewModel/RankListViewModel.m
@@ -0,0 +1,13 @@
+//
+//  RankListViewModel.m
+//  BilibiliComic
+//
+//  Created by 纪宇伟 on 2019/6/18.
+//  Copyright © 2019 jyw. All rights reserved.
+//
+
+#import "RankListViewModel.h"
+
+@implementation RankListViewModel
+
+@end
